7912d1b5728eaf6e2c2028063629d6ca83a9917f
SudhansuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2023-06-16 20:45:08 +0530

    🐛 (fix) : Fixed bug

--- client/src/components/Track.tsx
+++ client/src/components/Track.tsx
@@ -7,24 +7,24 @@ export default function Track({ trackId, trackImage, trackName, trackArtists, tr
     return (
         <div className="w-[100%] flex items-center justify-between">
             <div className="flex items-center lg:gap-5 md:gap-4 gap-3">
-                <Link to={`/track/${trackId}`} className='h-16 w-16 bg-gray-900'>
+                <Link to={trackName ? `/track/${trackId}` : ""} className='h-16 w-16 bg-gray-900'>
                     <img className="h-16 w-16" src={trackImage} alt={trackName} />
                 </Link>
 
                 <div>
-                    <Link to={`/track/${trackId}`}><p className="text-base font-semibold mt-2">{trackName}</p></Link>
+                    <Link to={trackName ? `/track/${trackId}` : ""}><p className="text-base font-semibold mt-2">{trackName}</p></Link>
                     <div className='flex flex-col'>
                         <p className="text-sm text-gray-400">By {trackArtists.map((artist: any, i: number) => (
-                        <React.Fragment key={artist.id}>
-                            {i > 0 && ', '}
-                            <Link className="hover:underline text-gray-400 hover:text-green-500" to={`/artist/${artist.id}`}>
-                                {artist.name}
-                            </Link>
-                        </React.Fragment>
-                    ))}</p> 
-                    {/* &nbsp;·&nbsp;  */}
-                    <Link className="hover:underline hover:text-green-500" to={`/album/${tractAlbumId}`}>
-                        <div className='text-sm hover:underline text-gray-500 hover:text-green-500 lg:w-[27rem] md:w-[24rem] w-[14rem] truncat'>{trackAlbum}</div></Link>
+                            <React.Fragment key={artist.id}>
+                                {i > 0 && ', '}
+                                <Link className="hover:underline text-gray-400 hover:text-green-500" to={`/artist/${artist.id}`}>
+                                    {artist.name}
+                                </Link>
+                            </React.Fragment>
+                        ))}</p>
+                        {/* &nbsp;·&nbsp;  */}
+                        <Link className="hover:underline hover:text-green-500" to={trackName ? `/album/${tractAlbumId}` : ""}>
+                            <div className='text-sm hover:underline text-gray-500 hover:text-green-500 lg:w-[27rem] md:w-[24rem] w-[14rem] truncat'>{trackAlbum}</div></Link>
                     </div>
                 </div>
             </div>
