a432c4513c0365606a0f2f3cae2a5f2cfb97170b
SudhansuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2023-06-16 19:36:20 +0530

    🐛 (fix) : Fixed Login Bug

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -8,7 +8,7 @@ const Login: FunctionComponent<LoginProps> = () => {
     return (
         <div className="flex-col min-h-screen flex items-center justify-center">
             <h1 className="text-4xl font-semibold">SpotiStat</h1>
-            <a className="inline-block bg-green-500 rounded-full px-8 py-2.5 mx-3 mt-8 min-w-min font-semibold text-white tracking-wider uppercase text-sm text-center hover:bg-green-600 focus:bg-green-600" href="http://localhost:8080/login">Login to Spotify</a>
+            <a className="inline-block bg-green-500 rounded-full px-8 py-2.5 mx-3 mt-8 min-w-min font-semibold text-white tracking-wider uppercase text-sm text-center hover:bg-green-600 focus:bg-green-600" href={`${import.meta.env.VITE_API_URL}/login`}>Login to Spotify</a>
         </div>
     );
 }
